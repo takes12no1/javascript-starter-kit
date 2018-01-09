ce2449ae32d44c884580ea24c25260b2a72ccfbb
Mark Coleman <takes12no1@gmail.com>
2018-01-09 09:18:01 -0600

    dependency updates

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,3 +1,21 @@
-var chalk = require('chalk');
+var express = require('express') ;
+var path = require('path') ;
+var open = require('open') ;
+// import path from 'path';
+// import open from 'open';
 
-console.log(chalk.green('Server Starting, if applicable...'));
+var port = 3000;
+var app = express();
+// var compiler = webpack(config);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
